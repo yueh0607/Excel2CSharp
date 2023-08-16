d1e8c0479a11b36b3e052213693192c3b8a18d68
yueh0607 <imyueh@foxmail.com>
2023-08-16 13:42:38 +0800

    x

---
+++ Excel2CS/ConfigTool/SyntaxStrategy/DynamicSyntaxAnalyser1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AirEditor.Config
+{
+    public class DynamicSyntaxAnalyser : ISyntaxAnalyser
+    {
+        public string TrueType => "System.Dynamic";
+        public object StringToValue(string str, string type, int row, int column)
+        {
+            return "\""+str+ "\"";
+        }
+    }
+}
